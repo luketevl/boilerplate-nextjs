7b812921a7907fcd75f124e1b6bfd700a3024ca8
Lukete Henrique <luketevl@gmail.com>
2021-01-14 15:21:00 -0300

    teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import Head from 'next/head'
 import styles from '../../styles/Home.module.css'
 
-const foo = 'dadada'
-
-export default function Home() {
+export default function Home                   () {
   return (
     <div className={styles.container}>
       <Head>
