ce9f7d36cae1ed4d5e2a0c0a4cb33e066022f9d5
Lukete Henrique <luketevl@Luketes-MacBook-Pro.local>
2021-01-14 23:07:50 -0300

    global style

---
+++ src/pages/_app.tsx
@@ -0,0 +1,20 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>React Avançado - Boilerplate</title>
+        <link rel="shortcut icon" href="/favicon.icon" />
+        <link rel="apple-shortcut-icon" href="/favicon.icon" />
+        <meta name="description" content="React Avançado para projetos" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
